b24d7024c8ab9cf3b8c977cd61cf01332a6359b9
lucaalessioo <lucaalessioo@hotmail.com>
2026-04-08 14:49:30 +0200

    Inizializzazione progetto finale

---
+++ progettofinale/progettofinale/src/main/java/it/itconsulting/progettofinale/ProgettofinaleApplication.java
@@ -0,0 +1,13 @@
+package it.itconsulting.progettofinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettofinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettofinaleApplication.class, args);
+	}
+
+}
---
+++ progettofinale/progettofinale/src/test/java/it/itconsulting/progettofinale/ProgettofinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itconsulting.progettofinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettofinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
